325bdf3373062c1d9187936efbe9a5ef73e9f48e
Chronique <adhi.chronique@gmail.com>
2026-01-08 22:53:45 +0700

    fix constant

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,8 +1,6 @@
-// 1. Alamat Contract
 export const CONTRACT_ADDRESS = "0x3C9b3A6A56204dC2149cCD93Cc5Fb01677b5B8a6";
 export const BUILDER_CODE_HEX = "62635f7667687139383365";
 
-// 2. ABI LENGKAP (Pastikan 'as const' tetap ada di paling bawah)
 export const CLASS_VOTE_ABI = [
   {
     "inputs": [
@@ -13,44 +11,7 @@ export const CLASS_VOTE_ABI = [
     "stateMutability": "nonpayable",
     "type": "constructor"
   },
-  {
-    "inputs": [{ "internalType": "address[]", "name": "_voters", "type": "address[]" }],
-    "name": "addToWhitelist",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "uint256", "name": "_candidateIndex", "type": "uint256" }],
-    "name": "vote",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "uint256", "name": "", "type": "uint256" },
-      { "internalType": "address", "name": "", "type": "address" }
-    ],
-    "name": "hasVotedInPoll",
-    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "pollId",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getFullWhitelist",
-    "outputs": [{ "internalType": "address[]", "name": "", "type": "address[]" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
+  // --- FUNGSI VIEW (Read Data) ---
   {
     "inputs": [],
     "name": "getCandidates",
@@ -71,9 +32,62 @@ export const CLASS_VOTE_ABI = [
   },
   {
     "inputs": [],
+    "name": "getFullWhitelist",
+    "outputs": [{ "internalType": "address[]", "name": "", "type": "address[]" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "pollId",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
     "name": "pollTitle",
     "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
     "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "pollCreated",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "", "type": "uint256" },
+      { "internalType": "address", "name": "", "type": "address" }
+    ],
+    "name": "hasVotedInPoll",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "name": "isAdmin",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  // --- FUNGSI WRITE (Write Data) ---
+  {
+    "inputs": [{ "internalType": "uint256", "name": "_candidateIndex", "type": "uint256" }],
+    "name": "vote",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address[]", "name": "_voters", "type": "address[]" }],
+    "name": "addToWhitelist",
+    "outputs": [],
+    "stateMutability": "nonpayable",
     "type": "function"
   },
   {
@@ -91,24 +105,20 @@ export const CLASS_VOTE_ABI = [
     "type": "function"
   },
   {
-    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "name": "isAdmin",
-    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
-    "stateMutability": "view",
+    "inputs": [
+      { "internalType": "string[]", "name": "_names", "type": "string[]" },
+      { "internalType": "string[]", "name": "_photos", "type": "string[]" }
+    ],
+    "name": "createPoll",
+    "outputs": [],
+    "stateMutability": "nonpayable",
     "type": "function"
   },
   {
-    "inputs": [],
-    "name": "pollCreated",
-    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "name": "whitelist",
-    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
-    "stateMutability": "view",
+    "inputs": [{ "internalType": "address", "name": "_admin", "type": "address" }],
+    "name": "addAdmin",
+    "outputs": [],
+    "stateMutability": "nonpayable",
     "type": "function"
   }
 ] as const;
