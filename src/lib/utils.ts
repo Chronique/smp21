516b38b75b26a47d551b6fc80ff8d11b252e4ba4
Chronique <adhi.chronique@gmail.com>
2026-01-18 17:51:09 +0700

    banner smpn21

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,8 @@ import { twMerge } from "tailwind-merge"
 export const METADATA = {
   name: "VOTE SMP 21", // Nama yang muncul di header Farcaster
   description: "Aplikasi Pemilihan Ketua Kelas Onchain SMP 21", // Deskripsi singkat
-  bannerImageUrl: 'https://i.imgur.com/2bsV8mV.png',
-  iconImageUrl: 'https://i.imgur.com/brcnijg.png',
+  bannerImageUrl: 'https://smp-21-jambi.vercel.app/logo-smp21.png',
+  iconImageUrl: 'https://smp-21-jambi.vercel.app/logo-smp21.png',
   // homeUrl: process.env.NEXT_PUBLIC_URL ?? 
   homeUrl: "https://smp-21-jambi.vercel.app",
   splashBackgroundColor: "#FFFFFF"
